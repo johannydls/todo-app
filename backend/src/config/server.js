84fe32134bd5eb2754314a748f2ac7d0c04d330a
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-05-03 17:19:36 -0300

    Ajusta CORS na API

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,14 @@
 const PORT = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
+const allowCors = require('./cors');
 
 const server = express();
 
 // Middlewares
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(PORT, () => {
   console.log(`Backend is running on  http://localhost:${PORT}`);
