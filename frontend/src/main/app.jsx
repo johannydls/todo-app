3ae426c08631900c646ff6b74bc9f44a0b5e2973
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-05-04 19:25:25 -0300

    Novos componentes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,8 +3,12 @@ import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
 
+import Todo from '../todo/Todo';
+import About from '../about/About';
+
 export default props => (
   <div className="container">
-    <h1>Teste</h1>
+    <Todo />
+    <About />
   </div>
 )
