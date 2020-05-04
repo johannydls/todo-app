fced9d7b3a7af3e6cf9db4756193be34b285c676
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-05-04 19:53:50 -0300

    Componente PageHeader

--- frontend/src/about/About.jsx
+++ frontend/src/about/About.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import PageHeader from '../template/PageHeader';
+
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <div>
+      <PageHeader name="Sobre" small="Nós" />
+
+      <h2>Nossa história</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Missão e visão</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Imprensa</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      
+    </div>
   </div>
 )
